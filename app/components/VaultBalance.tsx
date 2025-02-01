1b2efc37c6a43c2e263a7a941f42c84e62f8a5f4
Theo Premartin <theopremartin@MacBook-Pro-de-Theo.local>
2025-02-01 17:12:32 +0100

    corriger erreur

--- app/components/VaultBalance.tsx
+++ app/components/VaultBalance.tsx
@@ -1,6 +1,10 @@
 declare global {
     interface Window {
-      ethereum?: any;
+      ethereum?: {
+        request: (args: { method: string; params?: any[] }) => Promise<any>;
+        on: (eventName: string, callback: (...args: any[]) => void) => void;
+        removeListener: (eventName: string, callback: (...args: any[]) => void) => void;
+      };
     }
   }
   
